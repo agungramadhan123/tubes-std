e8da399ddfca4286fca6a93dd13c997d749cc711
Ariel Saradilla <168052088+bangsaini@users.noreply.github.com>
2024-11-25 10:47:02 +0700

    Update fungsi.cpp

--- fungsi.cpp
+++ fungsi.cpp
@@ -1,1 +1,36 @@
 #include "header.h"
+#include <iostream>
+using namespace std;
+
+void deletefirst(list& L, address& p) {
+    p = L.first;
+    if (L.first != nullptr) {
+        L.first = p->next;
+        p->next = nullptr;
+        L.first->prev = nullptr;
+    } else {
+        L.first = nullptr;
+        L.last = nullptr;
+    }
+}
+
+void deletelast(list& L, address& p) {
+    p = L.last;
+    if (L.first != L.last) {
+        L.last = p->prev;
+        L.last->next = nullptr;
+    } else {
+        L.first = nullptr;
+        L.last = nullptr;
+    }
+}
+
+void deleteafter(list& L, address& prec, address p) {
+    p = prec->next;
+    prec->next = p->next;
+    if (p->next != nullptr) {
+        p->next->prev = prec;
+    }
+    p->next = nullptr;
+    p->prev = nullptr;
+}
