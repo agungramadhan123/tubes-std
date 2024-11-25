51fdbce93c1aedb2e80a12dad4dab1ae659cbf3d
Ariel Saradilla <168052088+bangsaini@users.noreply.github.com>
2024-11-25 10:37:51 +0700

    Update header.h

--- header.h
+++ header.h
@@ -3,6 +3,28 @@
 #include <iostream> 
 using namespace std;
 
+typedef int infotype;
+typedef elmlist* address;
+
+struct elmlist {
+    infotype info;
+    address next;
+    address prev;
+};
+
+
+struct list {
+    address first;
+    address last;
+};
+
+void insertfirst(list& L, address p);
+void insertlast(list& L, address p);
+void insertafter(list& L, address prec, address p);
+void deletefirst(list& L, address& p);
+void deletelast(list& L, address& p);
+void deleteafter(list& L, address& prec, address p);
+void showall(list& L)
 
 
 #endif
