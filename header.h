3f02ed1f1a7f76ed6685b4b31105cdd0b2f3ab59
agung ramadhan <lortik4567@gmail.com>
2024-12-02 20:37:58 +0700

    Update header.h

--- header.h
+++ header.h
@@ -1,64 +1,66 @@
 #ifndef header_h_included
 #define header_h_included
-#include <iostream> 
+
+#include <iostream>
+#include <string>
+
 using namespace std;
-typedef int infotype;
+
+// DLL ADT
 typedef string infotype;
 typedef struct elmlist *address;
 
-const int MAXSIZE = 100; 
+// Stack ADT
+const int MAXSIZE = 100;
 typedef int Index;
+typedef struct elmstack *adr;
+
+struct elmstack {
+    infotype kata;
+    adr next;
+    adr prev;
+};
+
+struct listStack {
+    adr head = nullptr;
+    adr tail = nullptr;
+};
 
 struct stack {
     infotype info[MAXSIZE];
-    Index top;
+    Index top = -1;
 };
-
-//aril
-typedef char infotype;
-typedef elmlist* address;
 
 struct elmlist {
     infotype info;
-    address next;
-    address prev;
+    address next = nullptr;
+    address prev = nullptr;
 };
 
 struct list {
-    address first;
-    address last;
+    address first = nullptr;
+    address last = nullptr;
 };
 
+// aril
 void createlist(list &L);
 address createNewElm(infotype p);
+void deletefirst(list &L, address &p);
+void deletelast(list &L, address &p);
+void deleteafter(list &L, address &prec, address p);
 
-void deletefirst(list& L, address& p);
-void deletelast(list& L, address& p);
-void deleteafter(list& L, address& prec, address p);
-
-/*zikra*/
+// zikra
 void insertfirst(list &L, address p);
 void insertlast(list &L, address p);
 void insertafter(list &L, address prec, address p);
 void showall(list L);
 
-
 // agung
-stack create();
 bool isempty(stack s);
 bool full(stack s);
-void push(stack &s,infotype p);
-void pop(stack &s,infotype &p);
-void undo(stack &s, infotype x);
-void redo(stack &s,infotype x);
-
-
-<<<<<<< HEAD
+void push(stack &s, infotype p);
+void pop(stack &s, infotype &p);
+void undo(stack &undo, stack &redo, list &L);
+void redo(stack &undo, stack &redo, list &L);
 
 #endif
-=======
-// agung 
-void undo();
-void redo();
-#endif
->>>>>>> 7b5f0f73260645ce2bd2d7c8e8d561bceae973df
